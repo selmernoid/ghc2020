32d8da72f5efa118acc19916509342227e092477
Strunin Ihor <strunin@breeze-soft.com>
2020-02-20 20:50:12 +0200

    Models added

---
+++ Common/Library.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    public class Library
+    {
+        public int Id;
+        public int SignUpDays;
+        public int BooksPerDay;
+    }
+
+    public class Book
+    {
+        public int Id;
+        public int Score;
+    }
+
+}
