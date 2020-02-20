4a48013a2e925fcf1506e29e898124f8e1b3c513
Leonid Oleksyuk <l.oleksyuk@breeze-soft.com>
2020-02-20 23:00:16 +0200

    fix

---
+++ Common/SortedHash.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class OrderedList
+    {
+        private List<(int key, int value)> _list;
+
+        public OrderedList(IEnumerable<(int key, int value)> list)
+        {
+            _list = list.OrderBy(x => x.value).ToList();
+        }
+
+
+        public void Add((int key, int value) item)
+        {
+            for (int i = 0; i < _list.Count; i++)
+            {
+                if (item.value <= _list[i].value)
+                {
+                    _list.Insert(i, item);
+                    break;
+                }
+            }
+        }
+
+        public void Remove((int key, int value) value)
+        {
+            _list.Remove(value);
+        }
+    }
+
+}
