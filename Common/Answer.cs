28a38339c7b3f5d32778c91ca704cd87d5972096
anton <anton@breeze-soft.com>
2020-02-20 21:42:43 +0200

    .

---
+++ Common/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common {
+    public class Answer {
+        public List<LibraryAnswer> Libraries { get; set; }
+    }
+
+    public class LibraryAnswer
+    {
+        public int Id { get; set; }
+        public List<int> Books { get; set; }
+    }
+}
